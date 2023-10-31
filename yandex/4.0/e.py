96e6f9992e6b7b8d909b197dda3bac04bd318d34
Julia <yulu.2000@hotmail.com>
2023-10-31 21:06:33 +0300

    faster e solution

--- yandex/4.0/e.py
+++ yandex/4.0/e.py
@@ -2,13 +2,18 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-ans = []
-for i in range(n):
-	mark = arr[i]
-	s = 0
-	for j in range(n):
-		s += abs(arr[i] - arr[j])
+s = 0
 
-	ans.append(s)
+for i in range(len(arr)):
+	s += abs(arr[i] - arr[0])
+
+count_minus = len(arr) - 2
+count_plus = 0
+
+ans = [s]
+for i in range(1, n):
+	ans.append(ans[-1] - abs(arr[i] - arr[i-1]) * count_minus + abs(arr[i] - arr[i-1]) * count_plus)
+	count_plus += 1
+	count_minus -= 1
 
 print(*ans)
